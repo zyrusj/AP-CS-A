d5b6b0c51e3173db800bff7ec8e0fe396cd320f4
Zyrus Johnson <Zyrusmmfj@gmail.com>
2021-05-07 23:29:41 -0400

    intro lesson

---
+++ src/IntroAndPrimitives.java
@@ -0,0 +1,17 @@
+public class IntroAndPrimitives {
+
+    public static void main(String[] args) {
+        //This is a comment
+        /*
+        This is a multiline comment
+        It can span as long as you'd like
+         */
+
+        String firstString = "My string";
+        String first_string = "My string";
+        System.out.println(firstString);
+        System.out.println("String");
+
+
+    }
+}
