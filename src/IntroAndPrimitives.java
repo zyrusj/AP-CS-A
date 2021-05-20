6293bcd157a336f78a8c35d1df3999e6147de580
Zyrus Johnson <Zyrusmmfj@gmail.com>
2021-05-20 18:55:43 -0400

    added vocabulary

--- src/IntroAndPrimitives.java
+++ src/IntroAndPrimitives.java
@@ -15,3 +15,39 @@ public class IntroAndPrimitives {
 
     }
 }
+//For tutoring and test prep, email ZyrusJoh@gmail.com
+
+/*
+Vocabulary
+Computer Science (CS): used to encompass many aspects of computing, generally focused on development.
+CS fives you the tools to write statements (code) that will ideally make the user's experience
+both functional and enjoyable
+
+Programming style: the layout of your code including the way variables are named and
+
+Comments: This
+
+In-line/short comment: appear after or near a statement and are preceded by two forward slashes ("//")
+They have no impact on how your code functions but have notes for anyone that reads the code
+
+Long comment: a comment that spans multiple lines that. You can see this vocabulary block is a long comment
+
+Identifiers: names given to represent data store in memory. So variables names
+
+White space: open space that doesn't affect code added to enhance readability
+
+Interpreter: enables the computer to understand java code since it only understands binary
+
+Binary: code written in zeroes and ones. In java, your code is interpreter into binary
+
+Compiling: Interpreting from a programming language to binary
+
+Compile time error: If an error is found in the code from doing something the interpreter doesn't
+understand (like naming an identifier with the first character being a number) it will stop compiling and send you an error
+
+Logical error: An error where the code compiles correctly but doesn't give you the desired output. For example, a tip
+calculator that tells you 20% tip on $100 is $15. Your program didn't ran but it didn't give you the right output
+
+Run time error: An error that isn't caught by the compiler that occurs during the execution of the program. For example,
+trying to get the third item in a list of two items.
+ */
